a8fdbc36f4da0c090ada55ba730f8f418aa048ea
Kevin Zhang <zhangyongjiang@gmail.com>
2015-08-10 11:09:11 -0700

    JdbcUserDetailsManager

--- src/main/java/org/magnum/mobilecloud/video/repository/ProductApiService.java
+++ src/main/java/org/magnum/mobilecloud/video/repository/ProductApiService.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +34,7 @@ public class ProductApiService {
 
     @RequestMapping("/admin/add-user/{username}/{password}")
     public ResponseEntity<String> adminResource(Principal currentUser, @PathVariable("username") String username, @PathVariable("password") String password) {
-    	InMemoryUserDetailsManager userDetailsManager = (InMemoryUserDetailsManager) userDetailsService;
+    	JdbcUserDetailsManager userDetailsManager = (JdbcUserDetailsManager) userDetailsService;
     	userDetailsManager.createUser(new User(username, passwordEncoder.encode(password), new ArrayList<GrantedAuthority>()));
       	return new ResponseEntity<String>(currentUser.getName(), HttpStatus.OK);
 	}
